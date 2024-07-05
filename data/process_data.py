d6106464a70f636b9f447414d0fdaed2a6ea3a5d
Dare Kodjo Raoul Batcho <90199865+Raoul-Batcho@users.noreply.github.com>
2024-07-05 23:52:22 +0200

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,102 +1,90 @@
+from sqlalchemy import create_engine
 import sys
 import pandas as pd
-from sqlalchemy import create_engine
 
-def load_data(messages_filepath, categories_filepath):
+def load_data(messages_data_path, categories_data_path):
     """
-    INPUT:
-    messages_filepath - path to messages csv file
-    categories_filepath - path to categories csv file
-    
-    OUTPUT:
-    df - Merged data
+    This function takes two arguments, two files paths and load the data into the respective Pandas Dataframes and then merges both of the Dataframes.
+    It returns the Dataframe resulting from the merge function.
     """
-    messages = pd.read_csv(messages_filepath)
-    categories = pd.read_csv(categories_filepath)
-    df = pd.merge(messages, categories, on = 'id')
-    return df
+    messages_df = pd.read_csv(messages_data_path)
+    categories_df = pd.read_csv(categories_data_path)
+    merged_df = pd.merge(messages_df, categories_df, on = 'id')
+    return merged_df
 
 
-def clean_data(df):
+def clean_data(merged_df):
     """
-    INPUT:
-    df - Merged data
-    
-    OUTPUT:
-    df - Cleaned data
+    This fuction takes as argument the merged dataframe and retrns the result after cleaning.
     """
-    # create dataframe of the 36 individual category columns
-    categories = df['categories'].str.split(pat=';', expand = True)
     
-    # select the first row of the categories dataframe
+    # dataframe of the thirty six individual category columns
+    categories = merged_df['categories'].str.split(pat=';', expand = True)
+    
+    # look at the first row 
     row = categories.iloc[0]
     
-    # use this row to extract a list of new column names for categories
+    # extraction of a list of new column names for categories
     category_colnames = row.apply(lambda x:x[:-2])
     
-    # rename the columns of 'categories'
+    # renaming categories' columns
     categories.columns = category_colnames
     
-    # convert category values to just numbers 0 or 1
-    for column in categories:
-        # set each value to be the last character of the string
-        categories[column] = categories[column].astype(str).str[-1]
-        # convert column from string to numeric
-        categories[column] = categories[column].astype(int)
+    # converting category values only 0 or 1
+    for col in categories:
+       
+        categories[col] = categories[col].astype(str).str[-1]
+       
+        categories[col] = categories[col].astype(int)
     
-    # drop the original categories column from 'df'
-    df = df.drop('categories', axis = 1)
+    # Remove original categories column from 'merged_df'
+    merged_df = merged_df.drop('categories', axis = 1)
     
-    # concatenate the original datafram with the new 'categories' dataframe
-    df = pd.concat([df, categories], axis = 1)
+    # concatenation of the original dataframe with the new 'categories' dataframe
+    merged_df = pd.concat([merged_df, categories], axis = 1)
     
-    # drop duplicates
-    df.drop_duplicates(inplace = True)
+    # Remove duplicates
+    merged_df.drop_duplicates(inplace = True)
     
-    # drop 'child_alone' column as it has only 0 (ZERO) values - as per our jupyter Notebook analysis
-    df = df.drop('child_alone', axis = 1)
+    # Remove 'child_alone' column 
+    merged_df = merged_df.drop('child_alone', axis = 1)
     
-    # As per our Jupyter Notebook analysis, 'related' column has max value of 2, it could be error
-    # therefore, we will replace '2' with '1'
-    df['related'] = df['related'].map(lambda x: 1 if x==2 else x)
-        
-    return df
+    # Replace '2' with '1' in "related' column
+    merged_df['related'] = merged_df['related'].map(lambda x: 1 if x==2 else x)
+    
+    cleaned_df = merged_df 
+    return cleaned_df
 
 
-def save_data(df, database_filename):
+def save_to_sql(cleaned_df, database_filename):
     """
-    INPUT:
-    df - cleaned data
-    database_filename - database filename for sqlite database with (.db) file type
-    
-    OUTPUT:
-    None - save cleaned data into sqlite database
+    This function takes as argument, the cleaned dataframe and save cleaned data into sqlite database
     """
     engine = create_engine('sqlite:///'+ database_filename)
-    df.to_sql('DisasterResponse_table', engine, index = False, if_exists = 'replace')
+    cleaned_df.to_sql('DisasterResponse_table', engine, index = False, if_exists = 'replace')
 
 
 def main():
     if len(sys.argv) == 4:
 
-        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+        messages_data_path, categories_data_path, database_filepath = sys.argv[1:]
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
+        print(f'Loading data...\n    MESSAGES: {messages_data_path}\n    CATEGORIES: {categories_data_path}')
+              
+        df = load_data(messages_data_path, categories_data_path)
 
         print('Cleaning data...')
         df = clean_data(df)
         
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        print(f'Saving data...\n    DATABASE: {database_filepath}')
         save_data(df, database_filepath)
         
         print('Cleaned data saved to database!')
     
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
+        print('Please provide messages data filepath, then categories '\
+              'datasets filepath as the first and second argument respectively, as '\
+              'then the filepath of the database to save the cleaned data '\
               'to as the third argument. \n\nExample: python process_data.py '\
               'disaster_messages.csv disaster_categories.csv '\
               'DisasterResponse.db')
